860d8e89db10c74a465bdf12ce68973fd0ba9f8e
dbw7 <90856056+dbw7@users.noreply.github.com>
2023-03-18 06:21:09 -0400

    functioning websocket chat, needs to be cleaned up

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -103,6 +103,16 @@ func ListenToWsChannel() {
 			response.Action = "list_users"
 			response.ConnectedUsers = users
 			broadCastToAll(response)
+		case "left":
+			response.Action = "list_users"
+			delete(clients, event.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadCastToAll(response)
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", event.Username, event.Message)
+			broadCastToAll(response)
 		}
 		//response.Action = "Got here"
 		//response.Message = fmt.Sprintf("Some message and action was %s", event.Action)
@@ -112,7 +122,9 @@ func ListenToWsChannel() {
 func getUserList() []string {
 	var userList []string
 	for _, x := range clients {
-		userList = append(userList, x)
+		if x != "" {
+			userList = append(userList, x)
+		}
 	}
 	sort.Strings(userList)
 	return userList
